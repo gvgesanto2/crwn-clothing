4667f64f8a040cdee26ddba5aea3a997db26878d
Gabriel Santo <gvgesanto2@gmail.com>
2020-05-14 07:38:25 -0300

    fixed asking-to-fill-these-fields problem in google sign in button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -66,7 +66,11 @@ class SignIn extends React.Component {
             <CustomButton type="submit">
               Sign in
             </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton 
+              type="button" 
+              onClick={signInWithGoogle} 
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
