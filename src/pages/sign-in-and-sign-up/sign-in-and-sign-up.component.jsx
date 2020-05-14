399ec347ab3f3f24430bf93ab6dd6d7fe939b4e3
Gabriel Santo <gvgesanto2@gmail.com>
2020-05-14 07:25:13 -0300

    created sign up component, included ability to store new authenticated users into our database

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignOut = () => {
   return (
-    <div className="sign-in-and-sign-out">
+    <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
