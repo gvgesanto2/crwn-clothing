2a5f24d0a3b7ec970e16c225db0e370b1f1459fc
Gabriel Santo <gvgesanto2@gmail.com>
2020-05-14 10:47:38 -0300

    added redux, and implemented userReducer and userActions to navigationBar and app components

--- src/components/navigation-bar/navigation-bar.component.jsx
+++ src/components/navigation-bar/navigation-bar.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import { auth } from '../../firebase/firebase.utils';
+
 
 import { ReactComponent as Logo } from '../../assets/crwn.svg';
 import './navigation-bar.styles.scss'
@@ -36,4 +39,8 @@ const NavigationBar = ({ currentUser }) => {
   );
 }
 
-export default NavigationBar;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(NavigationBar);
